c4bd6a83d9a722e84fb5d71cc56f584d5e1bee15
Avi Levkovich <avi111@gmail.com>
2026-03-13 21:06:02 +0200

    css tips 13/3/26

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+import './index.css';
+
+const app = document.querySelector<HTMLDivElement>('#app');
+const mainLayout = document.querySelector<HTMLDivElement>('#main-layout');
+const closeBtn = document.getElementById('close-popup');
+const iframe = document.querySelector<HTMLIFrameElement>('iframe[name="content-frame"]');
+
+const closePopup = () => {
+    if (app) {
+        app.style.display = 'none';
+    }
+
+    if (mainLayout) {
+        mainLayout.style.display = 'flex';
+        // טעינת המבזק האחרון מהיום כברירת מחדל לאייפריים
+        if (iframe && !iframe.src) {
+           iframe.src = '/news/2026-03-13.html';
+        }
+    }
+};
+
+closeBtn?.addEventListener('click', closePopup);
+
+console.log('Happy developing ✨');
